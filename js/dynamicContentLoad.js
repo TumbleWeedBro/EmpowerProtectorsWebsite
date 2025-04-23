c12932edb177c4d36fe88f7946e198a02bbcca18
TumbleWeedBro <munashwengz@gmail.com>
2025-04-24 00:16:53 +0200

    templating using js attempt

--- js/dynamicContentLoad.js
+++ js/dynamicContentLoad.js
@@ -1,3 +1,14 @@
-function loadContent(content){
-    document.getElementById('content-placeholder').innerHTML= content;
+function loadContent(filePath, placeholderId){
+    fetch(filePath)
+    .then(response => {
+        if (!response.ok) throw new Error('Could not load $(filePath)');
+        return response.text();
+    })
+    .then(html => {
+        document.getElementById(placeholderId).innerHTML = html;
+    })
+    .catch(error => {
+        console.error(error);
+        document.getElementById(placeholderId).innerHTML = "<p>Error loading content</p>"
+    });
 }
